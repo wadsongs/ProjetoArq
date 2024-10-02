2279a138c1239fec7f3d8cb71520bd157d12073e
Wadson Gurgel <ad_gurgel@hotmail.com>
2024-10-02 17:28:43 -0300

    Classes 01

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+
+        a1.setNome("Fernando");
+
+        System.out.println(a1.getNome());
+    }
+}
